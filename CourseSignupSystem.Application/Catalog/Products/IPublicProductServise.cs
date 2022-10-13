f0041b624b237cc64d08af06a59505ea1efd6877
Quang-huy-26-04-2002 <huydangquang7@gmail.com>
2022-10-13 11:13:54 +0700

    first commit

---
+++ CourseSignupSystem.Application/Catalog/Products/IPublicProductServise.cs
@@ -0,0 +1,15 @@
+﻿using CourseSignupSystem.Application.Catalog.Dtos;
+using CourseSignupSystem.Application.Catalog.Products.Dtos.Public;
+using CourseSignupSystem.Application.Catalog.Products.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseSignupSystem.Application.Catalog.Products
+{
+    public interface IPublicProductServise
+    {
+        Task<PagedViewModel<ProducViewModel>> GetAllByCategory(GetProductPagingRequest reques);
+    }
+}
---
+++ CourseSignupSystem.Application/Catalog/Products/PublicProductServise.cs
@@ -0,0 +1,24 @@
+﻿using CourseSignupSystem.Application.Catalog.Dtos;
+using CourseSignupSystem.Application.Catalog.Products.Dtos;
+using CourseSignupSystem.Application.Catalog.Products.Dtos.Public;
+using CourseSignupSystem.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseSignupSystem.Application.Catalog.Products
+{
+    public class PublicProductServise : IPublicProductServise
+    {
+        private readonly CSSDbContext _context;
+        public PublicProductServise(CSSDbContext context)
+        {
+            _context = context;
+        }
+        public Task<PagedViewModel<ProducViewModel>> GetAllByCategory(GetProductPagingRequest reques)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
